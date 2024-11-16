2d5290baac6c785decacebf32e7882a1146cf3d8
Mladoneczki <padi.mladoneczki@gmail.com>
2024-11-16 12:39:37 +0100

    ronda nav, de hasznos

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ import Profil from './componentsPublic/Profil';
 import Products from './componentsPublic/Products';
 import Cart from './componentsPublic/Cart';
 import NavItem from './componentsPublic/NavItem';
+import Admin from './componentsAdmin/Admin';
+import ProductsA from './componentsAdmin/ProductsA';
+import Orders from './componentsAdmin/Orders';
+import Users from './componentsAdmin/Users';
 function App() {
   return (
     <BrowserRouter>
     <NavItem/>
         <div className="App">
+          
           <Routes>
               <Route  path="/home" element={<MainPage/>}/>
               <Route path="/profil" element={<Profil/>}/>
               <Route path ="/products"element={<Products/>}/>
               <Route path="/cart" element={<Cart/>}/>
+              <Route path="/productsA" element={<ProductsA/>}/>
+              <Route path="/users" element={<Users />} />
+              <Route path="/orders" element={<Orders/>}/>
+
+
           </Routes>
      
         </div>
--- src/componentsPublic/NavItem.js
+++ src/componentsPublic/NavItem.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
 
 function NavItem() {
-  return (
+  const [selectedNav, setSelectedNav] = useState(""); // Tárolja a választást: "" (kezdet), "home", "admin"
+
+ 
+  const initialMenu = (
+    <div>
+      <button  onClick={() => setSelectedNav("home")}>Home</button>
+      <button  onClick={() => setSelectedNav("admin")}>Admin</button>
+    </div>
+  );
+
+ 
+  const homeMenu = (
     <Nav defaultActiveKey="/home" as="ul">
       <Nav.Item as="li">
-        <Nav.Link as={Link} to="/home">Main</Nav.Link>
+        <Nav.Link as={Link} to="/home">Home</Nav.Link>
       </Nav.Item>
       <Nav.Item as="li">
         <Nav.Link as={Link} to="/profil">Profil</Nav.Link>
@@ -17,12 +28,38 @@ function NavItem() {
       <Nav.Item as="li">
         <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
       </Nav.Item>
+      <Nav.Item as="li">
+        <button  onClick={() => setSelectedNav("")}>Back</button>
+      </Nav.Item>
     </Nav>
+  );
+
+ 
+  const adminMenu = (
+    <Nav defaultActiveKey="/admin" as="ul">
+      <Nav.Item as="li">
+        <Nav.Link as={Link} to="productsA">Product</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link as={Link} to="/users">Users</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link as={Link} to="/orders">Orders</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <button  onClick={() => setSelectedNav("")}>Back</button>
+      </Nav.Item>
+    </Nav>
+  );
+
+ 
+  return (
+    <div>
+      {selectedNav === "" && initialMenu}
+      {selectedNav === "home" && homeMenu}
+      {selectedNav === "admin" && adminMenu}
+    </div>
   );
 }
 
-export default NavItem
-
-  
-
-
+export default NavItem;
